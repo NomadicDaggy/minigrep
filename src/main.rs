2eac241a2149983ce14eca564b65893fd3962b4b
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-29 18:41:18 +0200

    Save args, and add more sensible output

--- src/main.rs
+++ src/main.rs
@@ -3,6 +3,13 @@ use std::env;
 fn main() {
     // args returns an iterator of the cli arguments
     // To turn the iter into a collection, we use collect()
+    // Also, args will panic if we enter invalid unicode
     let args: Vec<String> = env::args().collect();
-    println!("{:?}", args);
+    
+    // Save args into variables
+    let query = &args[1];
+    let filename = &args[2];
+
+    println!("Searching for \"{}\"", query);
+    println!("In file       \"{}\"", filename);
 }
