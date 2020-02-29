b7519df78b19064c757501f85435c41d055dfea3
NomadicDaggy <dagsagrinbergs@gmail.com>
2020-02-29 18:51:47 +0200

    Add poem and read it from file

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,5 @@
 use std::env;
+use std::fs;
 
 fn main() {
     // args returns an iterator of the cli arguments
@@ -12,4 +13,10 @@ fn main() {
 
     println!("Searching for \"{}\"", query);
     println!("In file       \"{}\"", filename);
+
+    // -------- Read file
+    let contents = fs::read_to_string(filename)
+        .expect("Something went wrong reading the file");
+
+    println!("With text:\n{}", contents);
 }
